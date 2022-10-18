1889a9dad5218f43bba8888613889580325c2602
Adhitocho <adhit@dexter.id>
2022-10-18 22:06:28 +0700

    add table in database

--- app/routes.go
+++ app/routes.go
@@ -1,7 +1,11 @@
 package app
 
-import "github.com/adhityangot/js-be/app/controllers"
+import (
+	"github.com/adhityangot/js-be/app/controllers"
+	"github.com/gorilla/mux"
+)
 
 func (server *Server) InitializeRoutes() {
+	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
 }
--- app/server.go
+++ app/server.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -14,11 +17,45 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("Jajanan Suci is packing")
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
 
-	server.Router = mux.NewRouter()
+type DBConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
+	fmt.Println(appConfig.AppName + " is packing")
+
+	server.initializeDB(dbConfig)
 	server.InitializeRoutes()
+}
+
+func (server *Server) initializeDB(dbConfig DBConfig) {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
+	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed on connecting to the database server")
+	}
+
+	for _, model := range RegisterModels() {
+		err = server.DB.Debug().AutoMigrate(model.Model)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Database migration successful")
 }
 
 func (server *Server) Run(address string) {
@@ -26,9 +63,33 @@ func (server *Server) Run(address string) {
 	log.Fatal(http.ListenAndServe(address, server.Router))
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func Run() {
 	var server = Server{}
+	var appConfig = AppConfig{}
+	var dbConfig = DBConfig{}
 
-	server.Initialize()
-	server.Run(":9000")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error on loading .env file")
+	}
+
+	appConfig.AppName = getEnv("APP_NAME", "Jajanan Suci")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "9000")
+
+	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
+	dbConfig.DBUser = getEnv("DB_USER", "user")
+	dbConfig.DBPassword = getEnv("DB_PASSWORD", "password")
+	dbConfig.DBName = getEnv("DB_NAME", "dbname")
+	dbConfig.DBPort = getEnv("DB_PORT", "5432")
+
+	server.Initialize(appConfig, dbConfig)
+	server.Run(":" + appConfig.AppPort)
 }
